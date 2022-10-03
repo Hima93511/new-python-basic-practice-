12509ec5e2f23a58558ffd85cef2ed5d46d28624
Himanshu Garg <114793931+Hima93511@users.noreply.github.com>
2022-10-03 20:14:52 +0530

    Add files via upload

---
+++ some basic python.py
@@ -0,0 +1,23 @@
+ x = 'My '
+ y = 'name '
+ z = 'is'
+ print(x,y,z)
+ print(x + y + z)
+ x = y = z = 10
+ print(x,y,z)
+ x = 'my name is john'
+ print(x)
+ x = 10
+ y = 20
+ z = 30
+ print(x,y,z)
+ print(x + y + z)
+print('himanshu garg'* 100)
+a = 121
+b = 222
+print(a + b )
+print(a * b)
+x = 'John'
+def myfunc():
+    y = 'peter'
+    print("My name is " + x)
